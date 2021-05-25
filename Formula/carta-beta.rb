afda8f7c3c5d339a27669097900354c0bae5b9ad
ajm-asiaa <ajm@asiaa.sinica.edu.tw>
2021-05-25 10:03:39 +0800

    support for mojave and high_sierra

--- Formula/carta-beta.rb
+++ Formula/carta-beta.rb
@@ -4,13 +4,6 @@ class CartaBeta < Formula
   url "https://github.com/CARTAvis/carta-backend.git", tag: "v2.0.0-beta.0"
   license "GPL-3.0-only"
 
-  bottle do
-    root_url "https://github.com/CARTAvis/homebrew-tap/releases/download/carta-beta-2.0.0-beta.0"
-    rebuild 1
-    sha256 cellar: :any,                 catalina:     "ec196d2a6398fb66a3c5691540223ae24471c88ec5f5cfa60a792609fb6a0f80"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "7c596a1ec9d2dcf3ac9fc592896e48eca299c7f9e61634ad85fb14b682cb3d67"
-  end
-
   depends_on "cmake" => :build
   depends_on "cartavis/tap/carta-casacore"
   depends_on "cartavis/tap/zfp"
@@ -31,6 +24,8 @@ class CartaBeta < Formula
     sha256 "14d252e27eb2311fd44fbe17116eabe0faa37248ea5ba8e4c72183f882ff6d66"
   end
 
+  args << "-DUseBoostFilesystem=True" if MacOS.version <= :mojave
+
   def install
     # Building the carta-backend
     system "git", "submodule", "update", "--recursive", "--init"
@@ -40,7 +35,7 @@ class CartaBeta < Formula
       system "cmake", "..", "-DCMAKE_PREFIX_PATH=#{lib}",
                             "-DCMAKE_INCLUDE_PATH=#{include}",
                             "-DCMAKE_CXX_FLAGS=-I#{path}/casacode -I#{path}/casacore",
-                            "-DCMAKE_CXX_STANDARD_LIBRARIES=-L#{lib}", *std_cmake_args
+                            "-DCMAKE_CXX_STANDARD_LIBRARIES=-L#{lib}", *std_cmake_args, *args
       system "make", "install"
     end
     # Grabing the pre-built carta-frontend from the npm repository.
