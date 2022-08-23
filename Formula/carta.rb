3cee5970840d4c4bdee6f306c098fe1d7123b491
ajm-asiaa <ajm@asiaa.sinica.edu.tw>
2022-08-23 16:24:04 +0800

    updating the carta formula for the v3.0.0 release

--- Formula/carta.rb
+++ Formula/carta.rb
@@ -1,16 +1,14 @@
 class Carta < Formula
   desc "Backend and frontend components of CARTA"
   homepage "https://cartavis.github.io/"
-  url "https://github.com/CARTAvis/carta-backend.git", tag: "v2.0.0"
+  url "https://github.com/CARTAvis/carta-backend.git", tag: "v3.0.0"
   license "GPL-3.0-only"
 
   depends_on "cmake" => :build
-  depends_on "boost" if MacOS.version <= :mojave
   depends_on "cartavis/tap/carta-casacore"
   depends_on "cartavis/tap/zfp"
   depends_on "curl"
   depends_on "fmt"
-  depends_on "grpc"
   depends_on "hdf5@1.10"  
   depends_on "libomp"
   depends_on "libuv"
@@ -19,18 +17,12 @@ class Carta < Formula
   depends_on "pugixml"
   depends_on "wcslib"
   depends_on "zstd"
-  on_macos do
-    depends_on "tbb@2020"
-  end
-  on_linux do
-    depends_on "tbb"
-  end
 
   conflicts_with "carta-beta", because: "they both share the same executable name; 'carta'"
 
   resource "frontend" do
     url "https://registry.npmjs.org/carta-frontend/-/carta-frontend-2.0.0.tgz"
-    sha256 "c347dbd33466cd2f73a4f55e5b68a70573a82e5f0b7c0b1a10f1da91aeaeee21"
+    sha256 "56753940f0184cb66eb382032f8d82f425fc245e4b3656f5f38727582047e940"
   end
 
   def install
@@ -44,7 +36,6 @@ class Carta < Formula
       "-DCMAKE_CXX_FLAGS=-I#{path}/casacode -I#{path}/casacore",
       "-DCMAKE_CXX_STANDARD_LIBRARIES=-L#{lib}",
     ]
-    args << "-DUseBoostFilesystem=True" if MacOS.version <= :mojave
     mkdir "build-backend" do
       system "cmake", "..", *args, *std_cmake_args
       system "make", "install"
@@ -58,18 +49,17 @@ class Carta < Formula
 
   def caveats
     s = <<~EOS
-      CARTA officially supports the latest two MacOS versions; Catalina 10.15 and Big Sur 11.0.
+      CARTA officially supports the latest three MacOS versions; Catalina 10.15, Big Sur 11, and Monterey 12.
     EOS
     if MacOS.version <= :mojave
       s = <<~EOS
-        You are running MacOS #{MacOS.version}.
-        CARTA may still work on MacOS #{MacOS.version}, but it is untested by the CARTA team.
+        You are running MacOS #{MacOS.version}. CARTA can not run on MacOS #{MacOS.version}.
       EOS
     end
     s
   end
 
   test do
-    assert_match "2.0.0", shell_output("#{bin}/carta_backend --version")
+    assert_match "3.0.0", shell_output("#{bin}/carta_backend --version")
   end
 end
