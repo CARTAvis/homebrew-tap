2d801786ad8e581ae2b7bbb0da3c24aa2e2cf5e1
ajm-asiaa <ajm@asiaa.sinica.edu.tw>
2023-09-06 17:04:29 +0800

    updating formula to v4

--- Formula/carta.rb
+++ Formula/carta.rb
@@ -1,7 +1,7 @@
 class Carta < Formula
   desc "Backend and frontend components of CARTA"
   homepage "https://cartavis.github.io/"
-  url "https://github.com/CARTAvis/carta-backend.git", tag: "v3.0.1"
+  url "https://github.com/CARTAvis/carta-backend.git", tag: "v4.0.0"
   license "GPL-3.0-only"
 
   depends_on "cmake" => :build
@@ -14,22 +14,14 @@ class Carta < Formula
   depends_on "libuv"
   depends_on "pkg-config"
   depends_on "protobuf@21"
-  depends_on "pugixml"
   depends_on "wcslib"
   depends_on "zstd"
 
   conflicts_with "carta-beta", because: "they both share the same executable name; 'carta'"
 
   resource "frontend" do
-    url "https://registry.npmjs.org/carta-frontend/-/carta-frontend-3.0.0.tgz"
-    sha256 "56753940f0184cb66eb382032f8d82f425fc245e4b3656f5f38727582047e940"
-  end
-
-  # Fix to support cfitsio 4.2.0+
-  # See https://github.com/CARTAvis/carta-backend/pull/1232
-  patch do
-    url "https://gist.githubusercontent.com/ajm-asiaa/18244b2740b0ca69f7bf0464aaa2e614/raw/429611c2d896b8845ecc4114431842e9428bdb75/0000-Use-nullptr-or-cfitsio-length-constant-for-fits_read.patch"
-    sha256 "ddabeff06afe62fd095a155f71f17ebd5303645d755fecc8afc8eaaca087b065"
+    url "https://registry.npmjs.org/carta-frontend/-/carta-frontend-4.0.0.tgz"
+    sha256 "9dcf21bdc030d7779e9e8c7db7a4b8e8222336466368d559055b5187f1190183"
   end
 
   def install
@@ -41,8 +33,10 @@ class Carta < Formula
       "-DCMAKE_PREFIX_PATH=#{lib}",
       "-DCMAKE_INCLUDE_PATH=#{include}",
       "-DCMAKE_CXX_FLAGS=-I#{path}/casacode -I#{path}/casacore",
-      "-DCMAKE_CXX_STANDARD_LIBRARIES=-L#{HOMEBREW_PREFIX}/lib;-L#{lib}",      
+      "-DCMAKE_CXX_STANDARD_LIBRARIES=-L#{HOMEBREW_PREFIX}/lib;-L#{lib}",
+      "-DCARTA_CASACORE_ROOT=#{HOMEBREW_PREFIX}/Cellar/carta-casacore",
       "-DCartaUserFolderPrefix=.carta",
+      "-DDEPLOYMENT_TYPE=homebrew",
     ]
     mkdir "build-backend" do
       system "cmake", "..", *args, *std_cmake_args
@@ -57,7 +51,7 @@ class Carta < Formula
 
   def caveats
     s = <<~EOS
-      CARTA officially supports the latest three MacOS versions; Catalina 10.15, Big Sur 11, and Monterey 12.
+      CARTA officially supports the latest three MacOS versions; Big Sur 11, Monterey 12, and Ventura 13.
     EOS
     if MacOS.version <= :mojave
       s = <<~EOS
@@ -68,6 +62,6 @@ class Carta < Formula
   end
 
   test do
-    assert_match "3.0.1", shell_output("#{bin}/carta_backend --version")
+    assert_match "4.0.0", shell_output("#{bin}/carta_backend --version")
   end
 end
